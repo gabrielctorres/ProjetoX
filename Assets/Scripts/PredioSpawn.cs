7c0eae9c868c96229f9ec02936b6bb862672472c
Gabriel De Castro Torres <51802124+gabrielctorres@users.noreply.github.com>
2020-01-08 14:25:50 -0300

    0.2.1
    Sistema de spawn de predios adicionado

---
+++ Assets/Scripts/PredioSpawn.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PredioSpawn : MonoBehaviour
+{    
+    public List<GameObject> listaPredios = new List<GameObject>();
+    public int dropChance;
+    public float speedSpawn;
+    Vector2 posicaoSpawn;
+    GameObject predioAux;
+    void Start()
+    {
+        posicaoSpawn = transform.position;
+
+        InvokeRepeating("spawnPredio", 1f, speedSpawn);
+    }
+
+
+    void spawnPredio() { 
+    
+        int predioSorteado = Random.Range(0, listaPredios.Count);
+        posicaoSpawn.y = 0;
+        if (predioAux != null)
+        {
+            int distancia = Random.Range(20, 80);
+            SpriteRenderer spritePredio = predioAux.GetComponent<SpriteRenderer>();
+            posicaoSpawn.x += spritePredio.size.x * transform.localScale.x + distancia;
+            
+        }
+
+        predioAux = Instantiate(listaPredios[predioSorteado].gameObject, posicaoSpawn, Quaternion.identity);
+    }
+
+}
