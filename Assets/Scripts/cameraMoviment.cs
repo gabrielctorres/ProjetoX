6c42a6dc233ff93b426d61dd74b9550741592e18
Gabriel De Castro Torres <gabrielctorres38@gmail.com>
2020-07-20 20:12:24 -0300

    mudança camera parte 2

---
+++ Assets/Scripts/cameraMoviment.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMoviment : MonoBehaviour
+{
+    protected Rigidbody2D rb;
+    protected GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = Vector3.Distance(transform.position, player.transform.position);
+        if (dist > 29)
+        {
+            rb.velocity = transform.right * 20f;
+        }
+        else
+        {
+            rb.velocity = transform.right * 8f;
+
+        }
+    }
+}
